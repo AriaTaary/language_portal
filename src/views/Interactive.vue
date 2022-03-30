4a1314dafc45213e7f09f11b20031a8673d1042d
Shuman-Olga <78848580+Shuman-Olga@users.noreply.github.com>
2022-03-30 23:15:52 +0700

    Интеркативная карточка

---
+++ src/views/Interactive.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <VInteractiveCart></VInteractiveCart>
+    </div>
+</template>
+
+<script>
+import VInteractiveCart from '../components/v-interactive-cart'
+export default {
+  components: { VInteractiveCart },
+    name: 'Interactive',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
