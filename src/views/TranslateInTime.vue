acbaca4dca0e803b37d68ea255cfb256e15f747f
MukhametovUR <ural-lan4x4@yandex.ru>
2022-05-01 17:24:38 +0300

    проверка совподения

--- src/views/TranslateInTime.vue
+++ src/views/TranslateInTime.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
-    <input class="input" v-model="translate" type="text" />
+            <input class="input" v-model="translation" type="text" placeholder="Введите перевод"/>
 
-    <div v-for="item in sortArray" :key="item.id">
-      {{ item.translate }}
-    </div>
-
+    <v-simple-table class="table">
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Word</th>
+            <th class="text-left">Translate</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in search" :key="item.word" />
+          <tr v-for="item in array" :key="item.id" class="center">
+            <td align="center">{{ item.word }}</td>
+            <td v-show="item.active == true" class="correct" align="center">{{ item.translation }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
     <div class="text-center">
       <v-progress-circular
         :rotate="-90"
@@ -17,7 +30,9 @@
         {{ value }}
       </v-progress-circular>
       <v-btn @click="start">Start</v-btn>
+      
     </div>
+
   </div>
 </template>
 
@@ -27,15 +42,20 @@ export default {
     return {
       interval: {},
       value: 100,
-      translate: "",
-      isActive: false,
+      translation: '',
+      searchQuery: [],
       activeColor: "green",
       array: [
-        { id: 1, word: "мужчина", translate: "man", active: false },
-        { id: 2, word: "женщина", translate: "woman", active: false },
-        { id: 3, word: "дом", translate: "home", active: false },
-        { id: 4, word: "окно", translate: "window", active: false },
-
+        {id:1, word:"ox",translation:"бык",actve:  false},
+        {id:2, word:"owl",translation:"сова",actve:  false},
+        {id:3, word:"fox",translation:"лиса",actve:  false},
+        {id:4, word:"hare",translation:"заяц",actve:  false},
+        {id:5, word:"rat",translation:"крыса",actve:  false},
+        {id:6, word:"toad",translation:"жаба",actve:  false},
+        {id:7, word:"pig",translation:"свинья",actve:  false},
+        {id:8, word:"mole",translation:"крот",actve:  false},
+        {id:9, word:"cow",translation:"корова",actve:  false},
+        {id:10,word:"deer",translation:"олень",actve:  false}
       ],
     };
   },
@@ -53,17 +73,18 @@ export default {
     },
   },
   computed: {
-    sortArray() {
-      let comp = this.translate;
-      return  this.array.filter((elem) => {
-        if (comp === '') return true;
+    search() {
+      let comp = this.translation;
+      return (this.array.filter((elem) => {
+        if (comp === "") return true;
         else
           return (
-            elem.translate.indexOf(comp) > -1 &&
-            elem.translate.length == this.translate.length &&
-            this.array.splice((elem.active = true), elem.id)
-          );
-      });
+            elem.translation.indexOf(comp) > -1 &&
+            elem.translation.length == this.translation.length &&
+            this.array.includes((elem.active = true), elem.id) 
+            );
+      })
+      )     
     },
   },
 };
@@ -73,7 +94,19 @@ export default {
 .v-progress-circular {
   margin: 1rem;
 }
+.table {
+  margin: 0 200px;
+  border: 1px solid #a8b4d4;
+}
 .input {
-  border: 1px solid;
+  font-size: 16px;
+  display: flex;
+  margin: 10px auto;
+  border: 1px solid #a8b4d4;
+  justify-content: center;
+}
+
+.correct{
+  color: green;
 }
 </style>
